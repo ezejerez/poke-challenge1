a04c98de064c1d5fc3aebf1f3c10cc5c6346ac1f
Braidlockk <ezejerez99@gmail.com>
2021-06-15 19:42:00 -0300

    types fixes

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -19,12 +19,12 @@ export default function PokemonCard({ pokemon }) {
             })}
           </StyledType>
 
-          <StyledStats>
+          <div class='stats'>
             <p>HP: {pokemon.stats[0].base_stat}</p>
             <p>Attack: {pokemon.stats[1].base_stat}</p>
             <p>Defense: {pokemon.stats[2].base_stat}</p>
             <p>Abilities: {abilitiesNames.join(', ')}</p>
-          </StyledStats>
+          </div>
         </StyledBackCard>
       ) : (
         <StyledFrontCard>
@@ -48,124 +48,47 @@ const StyledPokemonCard = styled.div`
 
 const StyledFrontCard = styled.div`
   > img {
+    display: flex;
     height: 260px;
     background-color: white;
     border: 1px solid grey;
     margin-top: 30px;
     margin-left: 10px;
     margin-right: 10px;
-
-    display: flex;
   }
 
   > div {
+    display: flex;
+    justify-content: center;
     color: white;
     margin-top: 60px;
     font-size: 40px;
-
-    display: flex;
-    justify-content: center;
   }
 `;
 
 const StyledBackCard = styled.div`
   > div {
+    display: flex;
+    justify-content: center;
     color: white;
     margin-top: 30px;
     font-size: 30px;
-
-    display: flex;
-    justify-content: center;
   }
-`;
 
-const StyledStats = styled.p`
-  color: white;
-  margin-top: 25%;
-  margin-left: 5%;
-  font-size: 110%;
-
-  display: flex;
-  flex-direction: column;
+  .stats {
+    display: flex;
+    flex-direction: column;
+    color: white;
+    margin-top: 25%;
+    margin-left: 5%;
+    font-size: 110%;
+  }
 `;
 
 const StyledType = styled.div`
   > div {
     display: flex;
     justify-content: center;
-
-    .normal {
-      background-color: #bdbdaf;
-    }
-
-    .fighting {
-      background-color: #bf5540;
-    }
-
-    .flying {
-      background-color: #62a5ff;
-    }
-
-    .poison {
-      background-color: #c05da6;
-    }
-
-    .ground {
-      background-color: #fbcf49;
-    }
-
-    .rock {
-      background-color: #d4bd6e;
-    }
-
-    .bug {
-      background-color: #bdd300;
-    }
-
-    .ghost {
-      background-color: #7d76db;
-    }
-
-    .steel {
-      background-color: #c5c2dc;
-    }
-
-    .fire {
-      background-color: #ff5540;
-    }
-
-    .water {
-      background-color: #00afff;
-    }
-
-    .grass {
-      background-color: #59d842;
-    }
-
-    .electric {
-      background-color: #ffe619;
-    }
-
-    .psychic {
-      background-color: #ff64b9;
-    }
-
-    .ice {
-      background-color: #52f1ff;
-    }
-
-    .dragon {
-      background-color: #9376ff;
-    }
-
-    .dark {
-      background-color: #9a6853;
-    }
-
-    .fairy {
-      background-color: #ffaeff;
-    }
-
     color: white;
     margin-top: 1%;
     font-size: 70%;
@@ -176,13 +99,76 @@ const StyledType = styled.div`
     border-radius: 5px;
     margin: 1%;
   }
+
+  > div.normal {
+    background-color: #bdbdaf;
+  }
+
+  > div.fighting {
+    background-color: #bf5540;
+  }
+
+  > div.flying {
+    background-color: #62a5ff;
+  }
+
+  > div.poison {
+    background-color: #c05da6;
+  }
+
+  > div.ground {
+    background-color: #fbcf49;
+  }
+
+  > div.rock {
+    background-color: #d4bd6e;
+  }
+
+  > div.bug {
+    background-color: #bdd300;
+  }
+
+  > div.ghost {
+    background-color: #7d76db;
+  }
+
+  > div.steel {
+    background-color: #c5c2dc;
+  }
+
+  > div.fire {
+    background-color: #ff5540;
+  }
+
+  > div.water {
+    background-color: #00afff;
+  }
+
+  > div.grass {
+    background-color: #59d842;
+  }
+
+  > div.electric {
+    background-color: #ffe619;
+  }
+
+  > div.psychic {
+    background-color: #ff64b9;
+  }
+
+  > div.ice {
+    background-color: #52f1ff;
+  }
+
+  > div.dragon {
+    background-color: #9376ff;
+  }
+
+  > div.dark {
+    background-color: #9a6853;
+  }
+
+  > div.fairy {
+    background-color: #ffaeff;
+  }
 `;
-
-// useEffect(() => {
-//   const url = `https://pokeapi.co/api/v2/pokemon`;
-
-//   axios.get(url).then((result) => {
-//     setPokemonName(result.data.name);
-//     setPokemonImg(result.data.sprites.front_default);
-//   });
-// }, []);
